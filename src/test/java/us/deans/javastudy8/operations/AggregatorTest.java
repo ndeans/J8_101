8fba278e6513ea3a9e0d6fb977928604803da996
Deans,Nigel <nigel_deans2@vanguard.com>
2019-08-05 10:57:07 -0400

    added AggregatorTest, moved datafile

---
+++ src/test/java/us/deans/javastudy8/operations/AggregatorTest.java
@@ -0,0 +1,30 @@
+/*
+ ****************************************************************************
+ *
+ * Copyright (c)2019 The Vanguard Group of Investment Companies (VGI)
+ * All rights reserved.
+ *
+ * This source code is CONFIDENTIAL and PROPRIETARY to VGI. Unauthorized
+ * distribution, adaptation, or use may be subject to civil and criminal
+ * penalties.
+ *
+ ****************************************************************************
+ Module Description:
+
+ $HeadURL:$
+ $LastChangedRevision:$
+ $Author:$
+ $LastChangedDate:$
+*/
+package us.deans.javastudy8.operations;
+
+import org.junit.Test;
+
+public class AggregatorTest {
+
+	@Test
+	public void test_happypath() {
+		assert(true);
+	}
+	
+}
